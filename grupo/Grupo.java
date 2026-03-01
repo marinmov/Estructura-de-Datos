a58473308ead1b5cb05bef4c951e8af512bb19ac
marinmov <marlenemvleon99@gmail.com>
2026-03-01 00:57:09 -0600

    Refactor Grupo class and update comments

--- grupo/Grupo.java
+++ grupo/Grupo.java
@@ -1,5 +1,6 @@
 package grupo;
 /*
+Tecnologico Nacional de Mexico
 Instituto Tecnologico de Leon, Campus 1
 Carrera: Ingenieria en Sistemas Computacionales
 Materia: Estructura de Datos (Grupo B)
@@ -7,9 +8,10 @@ Horario:
     Lunes y Miercoles de 8:45 a 10:25
     Viernes de 8:45 a 9:35
 Alumno: Marlene Ines Moreno Velazquez
+Tarea #3
 Fecha: 25 de febrero de 2026
 
------ ALGORITMO -----
+----- ALGORITMO SECUENCIAL -----
 1. Meta: Mediante el programa se capturaran los datos de 25 personas (o menos)
         registrando su nombre, numero de control, fecha de nacimiento, peso y estatura.
         Posteriormente se buscara de manera secuencial una persona mediante su nombre.
@@ -17,7 +19,7 @@ Fecha: 25 de febrero de 2026
 2. Datos: 
     - Nombre: String nombre
     - Numero de control: String numControl
-    - Fecha de nacimiento: String fecha
+    - Fecha de nacimiento: DayArslan fecha
     - Peso: double peso
     - Estatura: double estatura
     Herramienta para la introduccion de datos y la salida de prompt: JOptionPane
@@ -28,6 +30,11 @@ Fecha: 25 de febrero de 2026
         el nombre a buscar con equalsIgnoreCase() que no hace distincion por mayusculas o minusculas.
         Esto al ser verdad, devuelve un valor booleano "true" e imprime el mensaje de exito, si nunca
         es verdad, devuelve un valor booleano "false" e imprime un mensaje de "Fracaso" o error.
+    - DayArslan.isValidDay(int dd, int mm, int yyyy) : boolean
+        Para validar la fecha y darle un formato ordenado se utiliza la clase DayArslan, creada por
+        una servidora, donde devuelve un valor booleano si la fecha es valida tomando en cuenta todos los
+        aspectos como el numero de dias de cada mes, y si el ano es bisiesto. Si es valido devuelve "true",
+        de lo contrario devuelve "false". 
 
 4. Resultados:
     - En caso de imprimir solo a la persona buscada: 
@@ -46,95 +53,109 @@ Fecha: 25 de febrero de 2026
     while (cuenta < grupo.length && getStop()!=1)
     
  */
+import java.awt.HeadlessException;
 import java.io.*;
 import javax.swing.JOptionPane;
 
-
 public class Grupo {
-    int cuenta; int stop = 0; int posicion;
-    String nombreb; 
+    int cuenta = 0; int stop = 0; int posicion;
+    String nombreb;
     boolean encontrado;
     int opcion;
     Persona grupo[] = new Persona[25];
-    
-   
+
+    void meta(){
+        String mensaje = """
+                Registro de personas mediante teclado y mediante lectura de archivo.
+                Usted puede registrar a maximo 25 personas, buscar a una persona en
+                especifico mediante su nombre e imprimir sus datos o la lista completa.
+                """;
+        JOptionPane.showMessageDialog(null, mensaje, "ALGORITMO SECUENCIAL", JOptionPane.PLAIN_MESSAGE);
+    }
+
     //2. DATA -> ENTRADA (registro de datos por teclado)
     void entrada(){
         String opciones[] = {"Continuar", "Terminar"};
-        cuenta = 0; setStop(0); boolean correct;
-         
+        setStop(0); boolean correct;
+
         while(cuenta<grupo.length && getStop()!=1){
             grupo[cuenta] = new Persona();
             String nombre, numControl;
-            
+
             double estatura, peso;
-            correct = false; 
+            correct = false;
             while(!correct){
-                nombre = JOptionPane.showInputDialog("Deme el nombre de la persona " + (cuenta+1)); 
+                nombre = JOptionPane.showInputDialog("Deme el nombre de la persona " + (cuenta+1));
                 correct = grupo[cuenta].setNombre(nombre);
                 if(!correct){
-                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!", 
-                    "ATENCION", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!",
+                            "ATENCION", JOptionPane.WARNING_MESSAGE);
                 }
             }
             correct = false;
             while(!correct){
-                numControl = JOptionPane.showInputDialog("Deme el numero de control de la persona " + (cuenta+1)); 
+                numControl = JOptionPane.showInputDialog("Deme el numero de control de la persona " + (cuenta+1));
                 correct = grupo[cuenta].setNControl(numControl);
                 if(!correct){
-                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!", 
-                    "ATENCION", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!",
+                            "ATENCION", JOptionPane.WARNING_MESSAGE);
                 }
             }
             correct = false;
             while(!correct){
                 try {
-                    String fecha = JOptionPane.showInputDialog("Deme la fecha de nacimiento de la persona " + (cuenta+1) + "\nUse dd/MM/yyyy"); 
-                    if(fecha == null) break;
-                    correct = grupo[cuenta].setFecha(fecha);
-                    
-                } catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, "Formato inválido. Use dd/MM/yyyy", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    int dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el dia de nacimiento",
+                            "FECHA DE NACIMIENTO", JOptionPane.INFORMATION_MESSAGE));
+                    int mes = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el mes de nacimiento numerico",
+                            "FECHA DE NACIMIENTO", JOptionPane.INFORMATION_MESSAGE));
+                    int year = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el anio de nacimiento",
+                            "FECHA DE NACIMIENTO", JOptionPane.INFORMATION_MESSAGE));
+                    correct = grupo[cuenta].setFecha(dia, mes, year);
+                    if(!correct) {
+                        JOptionPane.showMessageDialog(null, "Asegurese de introducir correctamente la fecha con numeros.",
+                                "FECHA INCORRECTA", JOptionPane.WARNING_MESSAGE);}
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Asegurese de introducir correctamente la fecha.\nUtilice UNICAMENTE numeros.",
+                            "FECHA INVALIDA", JOptionPane.WARNING_MESSAGE);
+                    correct = false;
                 }
             }
             correct = false;
             while(!correct){
                 try {
-                    estatura = Double.parseDouble(JOptionPane.showInputDialog("Deme la estatura de la persona " 
-                            + (cuenta+1)));  
+                    estatura = Double.parseDouble(JOptionPane.showInputDialog("Deme la estatura de la persona "
+                            + (cuenta+1)));
                     correct = grupo[cuenta].setEstatura(estatura);
                 } catch (NumberFormatException e) {
                     correct = false;
-                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!", 
-                    "ATENCION", JOptionPane.WARNING_MESSAGE);
-                }             
+                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!",
+                            "ATENCION", JOptionPane.WARNING_MESSAGE);
+                }
             }
             correct = false;
             while(!correct){
                 try {
-                    peso = Double.parseDouble(JOptionPane.showInputDialog("Deme el peso de la persona " + (cuenta+1))); 
+                    peso = Double.parseDouble(JOptionPane.showInputDialog("Deme el peso de la persona " + (cuenta+1)));
                     correct = grupo[cuenta].setPeso(peso);
-                } catch (Exception e) {
+                } catch (HeadlessException | NumberFormatException e) {
                     correct = false;
-                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!", 
-                    "ATENCION", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Campo OBLIGATORIO!",
+                            "ATENCION", JOptionPane.WARNING_MESSAGE);
                 }
             }
-            
             cuenta++;
-
-            stop = JOptionPane.showOptionDialog(null, 
-                "Selecciona una opcion", "REGISTRO DE DATOS", 
-                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, 
-                null, opciones, opciones[0]); 
-            
+            stop = JOptionPane.showOptionDialog(null,
+                    "Selecciona una opcion", "REGISTRO DE DATOS",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                    null, opciones, opciones[0]);
         }
         setCuenta(cuenta);
     }
+
     //2. DATA -> ENTRADA (registro de datos por lectura de archivo)
     void archivo(String archivoNombre){
         try (BufferedReader br = new BufferedReader(new FileReader(archivoNombre))) {
-            String line; 
+            String line;
             JOptionPane.showMessageDialog(null, "Leyendo de archivo...");
             while ((line = br.readLine()) != null && getCuenta()<grupo.length){
                 String[] datos = line.split(",");
@@ -148,16 +169,16 @@ public class Grupo {
                     cuenta++;
                 }
                 else {
-                    JOptionPane.showMessageDialog(null, "Formato incorrecto en linea: " 
-                                                + line, "ADVERTENCIA", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Formato incorrecto en linea: "
+                            + line, "ADVERTENCIA", JOptionPane.ERROR_MESSAGE);
                 }
             }
             setCuenta(cuenta);
-            JOptionPane.showMessageDialog(null, "Se han leido " + cuenta 
-                                        + " personas desde el archivo.");
+            JOptionPane.showMessageDialog(null, "Se han leido " + cuenta
+                    + " personas desde el archivo.");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al leer el archivo: " + e.getMessage(), 
-            "ERROR", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error al leer el archivo: " + e.getMessage(),
+                    "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -169,30 +190,30 @@ public class Grupo {
         setNombreB(nombreb);
         while(i<cuenta && !encontrado){
             if(grupo[i].getNombre().equalsIgnoreCase(getNombreb())){
-                JOptionPane.showMessageDialog(null, "Persona encontrada.\nNumero de preguntas realizadas: " 
-                                            + (i+1), "MENSAJE DE TERMINAL", JOptionPane.INFORMATION_MESSAGE); 
+                JOptionPane.showMessageDialog(null, "Persona encontrada.\nNumero de preguntas realizadas: "
+                        + (i+1), "MENSAJE DE TERMINAL", JOptionPane.INFORMATION_MESSAGE);
                 encontrado = true;
             }
             else{
                 i++;
-            } 
+            }
         }
         setPos(i); setEncontrado(encontrado);
         //4. RESULTADO -> Impresion (imprimir los datos de la persona buscada y numero de intentos realizados)
         if(getEncontrado()){
-            JOptionPane.showMessageDialog(null, grupo[getPos()].toString(), "ENCONTRADO", 
-            JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, grupo[getPos()].toString(), "ENCONTRADO",
+                    JOptionPane.INFORMATION_MESSAGE);
         } else if(!getEncontrado()){
-            JOptionPane.showMessageDialog(null, "No fue encontrado en el grupo.", 
-            "ENCONTRADO", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "No fue encontrado en el grupo.",
+                    "ENCONTRADO", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
     //4. RESULTADOS -> Impresion (imprimir la lista completa)
     void imprimir(){
-        if(getCuenta() == 0){ 
-            JOptionPane.showMessageDialog(null, "No hay personas registradas aún."); 
-            return; 
+        if(getCuenta() == 0){
+            JOptionPane.showMessageDialog(null, "No hay personas registradas aún.");
+            return;
         }
         int i = 0; String resultado ="";
         while(i<getCuenta()){
@@ -200,8 +221,8 @@ public class Grupo {
                     + "\n" + grupo[i].toString() + "\n";
             i++;
         }
-        JOptionPane.showMessageDialog(null, resultado, 
-            "LISTA DE TODAS LAS PERSONAS REGISTRADAS", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, resultado,
+                "LISTA DE TODAS LAS PERSONAS REGISTRADAS", JOptionPane.INFORMATION_MESSAGE);
 
     }
     //5. NAVEGABILIDAD -> Menu (solicitar al usuario seleccionar la accion deseada)
@@ -216,36 +237,42 @@ public class Grupo {
 
                 Eliga la opcion que desee...
                 """);
-        String opciones[] = {"Introducir datos", "Leer datos de archivo", "Buscar persona", 
-                            "Imprimir lista", "SALIR"};
+        String opciones[] = {"Introducir datos", "Leer datos de archivo", "Buscar persona",
+                "Imprimir lista", "SALIR"};
         int i = 0; encontrado = false;
-        setOpcion(JOptionPane.showOptionDialog(null, 
-                mensajeOpciones, "OPCIONES", 
-                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, 
+        setOpcion(JOptionPane.showOptionDialog(null,
+                mensajeOpciones, "OPCIONES",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                 null, opciones, opciones[0]));
         return getOpcion();
     }
 
-
     public static void main(String[] args) {
         Grupo mn = new Grupo(); mn.setOpcion(0);
+        mn.meta();
         int sw = mn.menu();
         while(sw != -1 && sw != 4){
             switch (sw) {
-                case 0: mn.entrada(); break;
-                case 1: mn.archivo("grupo/Personas.txt"); break;
-                case 2: mn.buscar(); break;
-                case 3: mn.imprimir(); break;
+                case 0 -> mn.entrada();
+                case 1 -> mn.archivo("C:\\Users\\marle\\Documents\\Estructura de Datos\\grupo\\Personas.txt");
+                case 2 -> mn.buscar();
+                case 3 -> mn.imprimir();
             }
             sw = mn.menu();
         }
     }
 
     //SETTERS Y GETTERS para datos y centinelas usados en los procesos
-    void setCuenta(int cuenta){this.cuenta=cuenta;} int getCuenta(){return cuenta;}
-    void setPos(int posicion){this.posicion = posicion;} int getPos(){return posicion;}
-    void setNombreB(String nombreb){this.nombreb = nombreb;} String getNombreb(){return nombreb;}
-    void setEncontrado(boolean encontrado){this.encontrado = encontrado;} boolean getEncontrado(){return encontrado;}
-    void setOpcion(int opcion){this.opcion = opcion;} int getOpcion(){return opcion;}
-    void setStop(int stop){this.stop = stop;} int getStop(){return stop;}
-}
+    void setCuenta(int cuenta){this.cuenta=cuenta;}
+    int getCuenta(){return cuenta;}
+    void setPos(int posicion){this.posicion = posicion;}
+    int getPos(){return posicion;}
+    void setNombreB(String nombreb){this.nombreb = nombreb;}
+    String getNombreb(){return nombreb;}
+    void setEncontrado(boolean encontrado){this.encontrado = encontrado;}
+    boolean getEncontrado(){return encontrado;}
+    void setOpcion(int opcion){this.opcion = opcion;}
+    int getOpcion(){return opcion;}
+    void setStop(int stop){this.stop = stop;}
+    int getStop(){return stop;}
+} 
